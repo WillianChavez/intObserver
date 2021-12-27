16e1a2054c1d79b6283dd5a82902cba263b7f62a
WillianChavez <williamchavezamaya@gmail.com>
2021-12-27 10:49:32 -0600

    setting observer for cards

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,42 @@ const url = 'https://picsum.photos/v2/list?page=2&limit=10'
 const cards = document.getElementById('cards')
 
 const generateCardString = ({ title, text, urlImage }) => {
-    const cardString = `<div class="card">
-    <div class="card__image">
-        <img src="${urlImage}" alt="" class="img" />
-    </div>
-    <div class="card__content">
-        <h2 class="card__title">${title}</h2>
-        <p class="card__text">${text}</p>
-    </div></div>`
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
 
-    return cardString
+    const cardImage = document.createElement('div')
+    cardImage.setAttribute('class', 'card__image')
+
+    const image = document.createElement('img')
+    image.setAttribute('src', urlImage)
+    image.setAttribute('alt', title)
+    image.setAttribute('class', 'img')
+
+    cardImage.appendChild(image)
+    card.appendChild(cardImage)
+
+    const cardContent = document.createElement('div')
+    cardContent.setAttribute('class', 'card__content')
+
+    const cardTitle = document.createElement('h2')
+    cardTitle.setAttribute('class', 'card__title')
+    cardTitle.textContent = title
+
+    const cardText = document.createElement('p')
+    cardText.setAttribute('class', 'card__text')
+    cardText.textContent = text
+
+    cardContent.appendChild(cardTitle)
+    cardContent.appendChild(cardText)
+    card.appendChild(cardContent)
+
+    return card
 }
 
 const getImages = async () => {
     const response = await fetch(url)
     const data = await response.json()
+    const fragment = document.createDocumentFragment()
     data.forEach((element) => {
         const objectCard = {
             title: element.author,
@@ -24,26 +45,44 @@ const getImages = async () => {
             urlImage: element.download_url,
         }
         const card = generateCardString(objectCard)
-        cards.innerHTML += card
+        fragment.appendChild(card)
     })
-
+    cards.appendChild(fragment)
     setObserver()
 }
-
 const setObserver = () => {
+    const optionsGetImages = {
+        root: null,
+        rootMargin: '300px 0px 0px 0px',
+        threshold: 0,
+    }
     const options = {
         root: null,
-        rootMargin: '150px 0px 0px 0px',
-        threshold: 0.5,
+        rootMargin: '100px 0px 0px 0px',
     }
-    const observer = new IntersectionObserver((entries) => {
+    const observerGetImages = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 getImages()
                 observer.unobserve(entry.target)
             }
+        }, cards.lastElementChild)
+    }, optionsGetImages)
+
+    observerGetImages.observe(cards.lastElementChild)
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show--card')
+            }
         })
+    }, options)
+
+    const cardsArray = document.querySelectorAll('.card')
+    cardsArray.forEach((card) => {
+        observer.observe(card)
     })
-    observer.observe(cards.lastElementChild)
 }
+
 getImages()
